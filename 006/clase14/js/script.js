a933a532ee3c45742439a674bbaea05e1719c314
PameGatica <pam.gatica@profesor.duoc.cl>
2021-04-29 16:55:12 -0400

    Semana 7 - Javascript y jQuery

---
+++ 006/clase14/js/script.js
@@ -0,0 +1,25 @@
+alert("Hola Mundo desde librería")
+console.log("Hola Mundo desde librería")
+
+function saludar(){
+    alert("Hola Mundo desde función")
+    console.log("Hola Mundo desde función")
+}
+
+
+function hablar(mensaje){
+    //alert(mensaje)
+    document.getElementById("h1Saludo").innerHTML = mensaje
+    console.log("Hombre dijo " + mensaje)
+}
+
+function sumar(){
+    var n1 = parseInt(document.getElementById("num1").value)
+    
+    var n2 = parseInt(document.getElementById("num2").value)
+
+    var suma = n1 + n2
+
+    document.getElementById("res").innerHTML = suma
+
+}
