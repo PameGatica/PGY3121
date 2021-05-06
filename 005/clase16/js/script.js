c8406ee4914b4f623c7af37eb4cdf609847ad270
PameGatica <pam.gatica@profesor.duoc.cl>
2021-05-06 13:38:19 -0400

    Clase 16 - Procesar data de API REST

--- 005/clase16/js/script.js
+++ 005/clase16/js/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+
+    $("#btnTodoChile").click(function(){
+        $.getJSON(
+            'https://api.gael.cloud/general/public/clima',
+            function(data){
+               $.each(data,function(i,item){
+                    $("#datos").append(
+                        "<tr>"+
+                            "<td>"+item.Codigo+"</td> "+
+                            "<td>"+item.Estacion+"</td> "+
+                            "<td>"+item.Estado+"</td> "+
+                            "<td>"+item.Temp+"°</td> "+
+                            "<td><button>Ver</button></td>"+
+                        "</tr>");
+
+               });
+            }
+        );
+
+    });
+
+
     $.getJSON(
         'https://api.gael.cloud/general/public/clima/SCVM',
         function(data){
